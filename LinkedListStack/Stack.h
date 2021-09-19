031af645dd26a7cf5e8c040bc0304834971876ab
Arthur Rambo Prediger <arthurpredigerrambo@gmail.com>
2021-09-19 17:41:13 -0300

    Stack implementation done

--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -2,9 +2,116 @@
 
 class Stack
 {
+private:
+	class Element
+	{
+	public:
+		Element(int value, Element* pNext)
+			:
+			value(value),
+			pNext(pNext)
+		{}
+		Element(const Element& source)
+			:
+			value(source.value)
+		{
+			if (source.pNext != nullptr)
+			{
+				pNext = new Element(*source.pNext);
+			}
+		}
+		~Element()
+		{
+			delete pNext;
+			pNext = nullptr;
+		}
+		Element& operator!=(const Element& rhs) = delete;
+		int GetValue() const
+		{
+			return value;
+		}
+		Element* Disconnect()
+		{
+			auto temp = pNext;
+			pNext = nullptr;
+			return temp;
+		}
+		int CountElements() const 
+		{
+			if (pNext != nullptr)
+			{
+				return pNext->CountElements() + 1;
+			}
+			else
+			{
+				return 1;
+			}
+		}
+	private:
+		Element* pNext = nullptr;
+		int value;
+	};
 public:
-	void Push( int val );
-	int Pop();
-	int Size() const;
-	bool Empty() const;
+	Stack() = default;
+	Stack(const Stack& s)
+	{
+		*this = s;
+	}
+	Stack& operator=(const Stack& s)
+	{
+		if (&s != this)
+		{
+			if (!Empty())
+			{
+				delete pTop;
+				pTop = nullptr;
+			}
+			if(!s.Empty())
+			{
+				pTop = new Element(*s.pTop);
+			}
+		}
+		return *this;
+	}
+	~Stack()
+	{
+		delete pTop;
+		pTop = nullptr;
+	}
+	void Push(int value)
+	{
+		pTop = new Element(value, pTop);
+	}
+	int Pop()
+	{
+		if (!Empty())
+		{
+			auto pOldTop = pTop;
+			pTop = pTop->Disconnect();
+			int temp = pOldTop->GetValue();
+			delete pOldTop;
+			return temp;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	int Size() const
+	{
+		if (!Empty())
+		{
+			return pTop->CountElements();
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	bool Empty() const
+	{
+		return pTop == nullptr;
+	}
+private:
+	Element* pTop = nullptr;
 };
